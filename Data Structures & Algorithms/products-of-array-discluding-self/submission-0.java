def3e9c064957c4c679c04743af11033e24fa7ab
Evan Nguyen <146302931+EvanTN@users.noreply.github.com>
2026-05-19 13:27:27 -0400

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+    
+        int[] prefix = new int[nums.length];
+        int[] postfix = new int[nums.length];
+        int[] res = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            if(i == 0){
+                prefix[i] = nums[i];
+            } else {
+                prefix[i] = nums[i] * prefix[i-1];
+            }
+        }
+
+        for(int i = nums.length - 1; i >= 0; i--){
+            if(i == nums.length - 1){
+                postfix[i] = nums[i];
+            } else{
+                postfix[i] = nums[i] * postfix[i+1];
+            }      
+        }
+
+
+        for(int i = 0; i < nums.length; i++){
+            if(i == 0){
+                res[i] = postfix[i+1];
+            }else if(i == nums.length - 1){
+                res[i] = prefix[i-1];
+            }else{
+            res[i] = prefix[i-1] * postfix[i+1];
+            }
+        }
+
+        return res;
+    }
+}  
